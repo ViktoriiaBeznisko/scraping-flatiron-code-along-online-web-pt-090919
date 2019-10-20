4c4ae6f17a583550c3a115ca735bb9ba5031776f
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-20 03:34:11 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,22 +1,52 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
-  
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title && course.title != ""
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
+
+  def get_page
+
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+
+    #doc.css(".post").each do |post|
+      #course = Course.new
+      #course.title = post.css("h2").text
+      #course.schedule = post.css(".date").text
+      #course.description = post.css("p").text
+    #end
   end
-  
+
+  def get_courses
+    self.get_page.css(".post")
+  end
+
+
 end
 
 
 
+# the collection of course offerings:
+# doc.css(".post")
+
+# The title of an individual course offering:
+# doc.css(".post").first.css("h2").text
+
+# The schedule of an individual course offering:
+# doc.css(".post").first.css(".date").text
+
+# The description of an individual course offering:
+# doc.css(".post").first.css("p").text
+
+
+# def print_courses
+#   self.make_courses
+#   Course.all.each do |course|
+#     if course.title && course.title != ""
+#       puts "Title: #{course.title}"
+#       puts "  Schedule: #{course.schedule}"
+#       puts "  Description: #{course.description}"
+#     end
+#   end
+# end
